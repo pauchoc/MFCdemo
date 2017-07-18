3682e81b1c4daa397bf361120d55778bb1cc3800
Marina <Marina@HP>
2017-07-18 12:13:34 +0300

    Добавьте файлы проекта.

---
+++ MFCdemo/MFCdemo.h
@@ -0,0 +1,32 @@
+
+// MFCdemo.h : главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CMyApp:
+// О реализации данного класса см. MFCdemo.cpp
+//
+
+class CMyApp : public CWinApp
+{
+public:
+	CMyApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMyApp theApp;
